423f6cf0194b88c38c54d6da2ce8e6923cf10aa2
Hargreaves <nh103043@bgov.com>
2019-05-31 16:30:58 -0400

    hooking up to vendor api

---
+++ test_requests.py
@@ -0,0 +1,7 @@
+import requests
+
+response = requests.get('https://www.bgov.com/companies_api/v1/vendors/10548125.json',
+headers ={'Authorization': 'Token token="eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7ImlkIjozNzgzNiwiZGV2ZWxvcGVyIjoiOTk2YzAyOWUtYWI2NC00MjRmLTg4ZDUtYTY1ZTViYTYyYjVlIn19.kA9Cb9576QK7E1I5lVcMy8-xKG-EVmnEcu8hKdeAa7I"'}
+)
+
+
